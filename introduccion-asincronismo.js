cefa433e013df9152e4c64aa890014f73b72318b
JulianCode <hijuliansosa@protonmail.com>
2020-05-15 14:58:15 -0500

    Introducción al asincronismo

---
+++ introduccion-asincronismo.js
@@ -0,0 +1,10 @@
+
+// ASINCRONISMO
+/**
+ * - Memory Heap: Es el espacio de memoria compartido para toda nuestra aplicación
+ * - Pila de ejecución CALLSTACK. Donde nuestras fucniones van a ser puestas en orden de ejecución
+ * - Cola de tareas: Las tareas se encolan según el orden que van llegando:
+ * - Callback: Una función que JS ejecutara cuando regrese la respuesta del servidor
+ * - Callback Queue: Cola de callbacks esperando a ser ejecutadas
+ * ⚠ Solo hasta que el programa se quede sin funcinoes en la pila de ejecución es que va a ir a buscar las 
+ */
