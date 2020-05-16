869fd5606e1895960b53f4514e98b29158938f67
JulianCode <hijuliansosa@protonmail.com>
2020-05-16 09:46:40 -0500

    Implementando Promesas

---
+++ src/promises/index.js
@@ -0,0 +1,49 @@
+const somethingWillHappen = () => {
+  return new Promise((resolve, reject) => {
+    const data = { "code": null, "message": null, }
+    if (true) {
+      this.data = {
+        "code": 1,
+        "message": 'Hey!'
+      }
+      return resolve(this.data)
+    } else {
+      this.data = {
+        "code": 0,
+        "message": 'Whooops!'
+      }
+      return reject(this.data)
+    }
+  })
+}
+
+somethingWillHappen()
+  .then(response => console.log(response))
+  .catch(error => console.log(error))
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      setTimeout(() => {
+        resolve('Hey!')
+      }, 2000);
+    } else {
+      const error = new Error('Whooops!');
+      return reject(error);
+    }
+  })
+}
+
+somethingWillHappen2()
+  .then(response => console.log(response))
+  .catch(err => console.error(err))
+
+// Promise All
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then(results => {
+    console.log('Promise All', results)
+  })
+  .catch(err => {
+    console.error('Promise All', err)
+  })
