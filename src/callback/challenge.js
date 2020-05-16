c08a5acf18412ff78f12509be3a3a8ac70d54205
JulianCode <hijuliansosa@protonmail.com>
2020-05-16 09:00:24 -0500

    Múltiples Peticiones a un API con Callbacks

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,9 +1,10 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback) {
   let xhttp = new XMLHttpRequest();
   xhttp.open('GET', url_api, true);
-  xhttp.onreadystatechange = (event) => {
+  xhttp.onreadystatechange = function(event) {
     if (xhttp.readyState === 4) {
       if (xhttp.status === 200) {
         callback(null, JSON.parse(xhttp.responseText));
@@ -15,3 +16,16 @@ function fetchData(url_api, callback) {
   }
   xhttp.send();
 }
+
+fetchData(API, (error1, data1) => {
+  if (error1) return console.log(error1);
+  fetchData(API + data1.results[0].id, function (error2, data2) {
+    if (error2) return console.log(error2);
+    fetchData(data2.origin.url, function (error3, data3) {
+      if (error3) return console.log(error3);
+      console.log('Data1', data1.info.count)
+      console.log('Data2', data2.name)
+      console.log('Data3', data3.dimension)
+    })
+  })
+})
