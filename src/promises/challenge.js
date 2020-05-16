8e4b3d11fc1c8d6fbb1df66df96e8ca8b8174fe3
JulianCode <hijuliansosa@protonmail.com>
2020-05-16 11:06:41 -0500

    Resolver problema con Promesas

---
+++ src/promises/challenge.js
@@ -0,0 +1,20 @@
+const fetchData = require('../utils/fetchData')
+let API = 'https://rickandmortyapi.com/api/character/';
+
+fetchData(API)
+  .then(data => {
+    console.log(data.info.count)
+    console.log(data)
+    return fetchData(`${API}${data.results[0].id}`)
+  })
+  .then(data => {
+    console.log(data.name)
+    console.log(data)
+    return fetchData(`${data.origin.url}`)
+  })
+  .then(data => {
+    console.log(data.dimension)
+    console.log(data)
+    return data.dimension
+  })
+  .catch(err => console.error(err))
